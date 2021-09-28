0d4e66a1b088e63246fecba89134906e44e5275c
kimdohy <134ehgud@naver.com>
2021-09-28 11:32:28 +0900

    list, delete, update 명령어 구현 완료

--- src/AddrApp.java
+++ src/AddrApp.java
@@ -8,7 +8,7 @@ public class AddrApp {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
@@ -16,60 +16,99 @@ public class AddrApp {
 		String url = "jdbc:mysql://localhost:3306/b1?serverTimezone=UTC";
 		String user = "root";
 		String pass = "";
-		
+
 		try {
-			
+
 			// 1. 드라이버 세팅
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			
+
 			// 2. 커넥션 획득
 			conn = DriverManager.getConnection(url, user, pass);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("DB작업중 문제 발생");
 		}
-		
-		
-		while(true) {
+
+		while (true) {
 			System.out.print("명령어를 입력해주세요 : ");
-			
-			String command = sc.nextLine();
-			
+
+			String command = sc.next();
+
 			if (command.equals("exit")) {
 				System.out.println("시스템을 종료합니다.");
 				break;
 			}
 			switch (command) {
-			case "add":
+			case "add" :
 				System.out.println("======== 주소록 등록 ========");
-				
+
 				System.out.print("이름 : ");
 				String name = sc.nextLine();
 				System.out.print("주소 : ");
 				String addr = sc.nextLine();
 				System.out.print("전화번호 : ");
 				String tel = sc.nextLine();
-				
+
 				try {
 					stmt = conn.createStatement();
-					String sql = "INSERT INTO article\r\n"
-							+ "SET `name` = '" + name + "',\r\n"
-							+ "    address = '" + addr + "',\r\n"
-							+ "    tel = '" + tel + "';";
+					String sql = "INSERT INTO article\r\n" + "SET `name` = '" + name + "',\r\n" + "    address = '"
+							+ addr + "',\r\n" + "    tel = '" + tel + "';";
 					stmt.executeUpdate(sql);
-				}catch(Exception e) {
-					System.out.println("작업중 문제 발생");
+				} catch (Exception e) {
+					System.out.println("add 명령어 작업중 문제 발생");
 				}
-				System.out.println("======== 주소록 등록 완료 ========\n");				
+				System.out.println("======== 주소록 등록 완료 ========\n");
 				break;
 			case "update":
-				System.out.println("update 명령어 실행");
+				System.out.print("몇번 주소록을 수정하시겠습니까 : ");
+				int id = sc.nextInt();
+				System.out.print("이름 : ");
+				name = sc.next();
+				System.out.print("주소 : ");
+				addr = sc.next();
+				System.out.print("전화번호 : ");
+				tel = sc.next();
+				try {
+					stmt = conn.createStatement();
+					String sql = "UPDATE article \r\n" + "SET `name` = '" + name + "',\r\n" + "     address = '" + addr
+							+ "',\r\n" + "     tel = '" + tel + "'\r\n" + "WHERE id = " + id + ";";
+					stmt.executeUpdate(sql);
+				} catch (Exception e) {
+					System.out.println("update 명령어 작업중 문제 발생");
+				}
+				System.out.println("수정이 완료되었습니다.");
 				break;
 			case "delete":
-				System.out.println("delete 명령어 실행");
+				System.out.print("몇번 주소록을 삭제하시겠습니까 : ");
+				id = sc.nextInt();
+				try {
+					stmt = conn.createStatement();
+					String sql = "delete from article\r\n"
+							+ "where id = " + id + ";";
+					stmt.executeUpdate(sql);
+				}catch(Exception e) {
+					System.out.println("delete 명령어 실행중 문제 발생");
+				}
+				System.out.println(id + "번 주소록이 삭제되었습니다.");
+				
 				break;
 			case "list":
-				System.out.println("list 명령어 실행");
-				
+				System.out.println("========= 주소록 목록 =========");
+
+				try {
+					stmt = conn.createStatement();
+					String sql = "SELECT * FROM article";
+					rs = stmt.executeQuery(sql);
+
+					while (rs.next()) {
+						System.out.println("번호 : " + rs.getInt("id"));
+						System.out.println("이름 : " + rs.getString("name"));
+						System.out.println("주소 : " + rs.getString("address"));
+						System.out.println("전화번호 : " + rs.getString("tel") + "\n");
+					}
+				} catch (Exception e) {
+					System.out.println("list 명령어 작업중 문제 발생");
+				}
+
 				break;
 			case "search":
 				System.out.println("serch 명령어 실행");
@@ -78,7 +117,7 @@ public class AddrApp {
 				System.out.println("없는 명령어 입니다.");
 			}
 		}
-		
+
 		sc.close();
 	}
 
